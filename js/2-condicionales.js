51a8e9c0cafe0f29e3220979aa152f32c3f9f432
Juan <juancir18@gmail.com>
2024-10-11 17:17:40 -0300

    Hicimos el ejercicio2 de js

---
+++ js/2-condicionales.js
@@ -0,0 +1,26 @@
+// estructuras condicionales if
+
+
+// if (condicion logica){
+// ....
+// } else{
+    // ...
+// }
+
+// solicitar la edad al usuario e indicar por pantalla si puede votar o no
+
+// tengo que pedirle la edad al usuario
+// puede votar a partir de los 16 años
+// parseInt numero entreo
+// parseFloat numero con decimal
+let edad = parseInt(prompt('Ingrese su edad'));
+
+
+console.log(typeof(edad));
+console.log(edad);
+
+if (edad >= 16){
+    document.write('usted debe votar');
+}else{
+    document.write('no puede votar');
+}
