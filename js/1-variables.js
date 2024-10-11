bdf52f6b22fb5fc8331b96eb5440967b4e0c6549
Juan <juancir18@gmail.com>
2024-10-11 16:58:58 -0300

    practicamos con variables, document.write y alert

---
+++ js/1-variables.js
@@ -0,0 +1,20 @@
+// declarar una variable (var, let, const)
+let nombreUsuario = 'Juan Pascual';
+const anio = 2023;
+
+// mostrar por consola una variable 
+console.log('Hola Mundo');
+console.log(nombreUsuario);
+console.log(nombreUsuario + ' es alumno del año ' + anio);
+console.log(`${nombreUsuario} es alumno del año ${anio}`);
+
+// mostrar un mensaje en el doc. HTML
+ document.write(nombreUsuario + ' es alumno del año ' + anio);
+document.write(`<h1>Hola mundo</h1>`);
+//  mostrar un mensaje en una ventana emergente 
+alert('este es mi primer alert');
+
+// modificar el valor de una variable
+nombreUsuario = 'Pascual Juan';
+document.write(nombreUsuario + ' es alumno del año ' + anio);
+document.write('<br> esto es otra prueba');
